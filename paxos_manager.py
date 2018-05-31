aa45287adc1fa8265bbefb042d72b977aec83856
dannycho7 <dannycho7@gmail.com>
2018-05-30 23:52:43 -0700

    replace p1lock with general purpose lock on PaxosManager

--- paxos_manager.py
+++ paxos_manager.py
@@ -14,21 +14,22 @@ class PaxosManager:
 		self.isLeader = False
 		self.acks = [] # should contain items of: { 'acceptNum': <int>, 'acceptVal': <list> }
 		self.pid = globalConfig[serverI]['id']
-		self.p1lock = threading.Lock() # phase 1 lock
+		self.lock = threading.Lock() # reduces complexity by only processing one message at a time/preventing a collision from starting an election and processing a msg at the same time.
 	def broadcast(self, msg):
 		clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		for broadcastAddr in self.globalConfig:
 			clientSocket.sendto(msg, (broadcastAddr['ip_addr'], broadcastAddr['port']))
 	def init_election(self):
-		self.p1lock.acquire()
+		self.lock.acquire()
 		self.acks = []
 		self.acceptVal = [] # change this to some kind of get items from queue function
 		self.ballotNum = { 'num': self.ballotNum['num'] + 1, 'pid': self.pid ,'depth': self.depth}
 		self.electionInProg = True
 		self.isLeader = False
 		self.broadcast(create_prepare_msg(self.pid, self.ballotNum))
-		self.p1lock.release()
+		self.lock.release()
 	def process_recv_msg(self, msg):
+		self.lock.acquire()
 		safe_print("Received message: {0}".format(str(msg)))
 		if msg['header']['type'] == 'accept':
 			self.process_accept_msg(msg)
@@ -38,10 +39,10 @@ class PaxosManager:
 			self.process_ack_msg(msg)
 		else:
 			raise Error('Incorrect msg format' + str(msg))
+		self.lock.release()
 	def process_accept_msg(self, msg):
 		pass
 	def process_ack_msg(self, msg):
-		self.p1lock.acquire()
 		if self.isLeader or not self.electionInProg:
 			return # if you already won the election or there is no election in progress, then ignore this ack msg
 		if msg['header']['ballotNum'] == self.ballotNum:
@@ -50,9 +51,7 @@ class PaxosManager:
 			self.isLeader = True
 			val = self.__get_accept_val_from_acks()
 			self.broadcast(create_accept_msg(self.pid, self.ballotNum, val))
-		self.p1lock.release()
 	def process_prepare_msg(self, msg):
-		self.p1lock.acquire()
 		pid = msg['header']['pid']
 		ballotNum = msg['header']['ballotNum']
 		if ballotNum['depth'] < self.depth:
@@ -67,7 +66,6 @@ class PaxosManager:
 			ack_msg = create_ack_msg(self.pid, self.ballotNum, self.acceptNum, self.acceptVal)
 			clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 			clientSocket.sendto(ack_msg, (self.globalConfig[pid]['ip_addr'], self.globalConfig[pid]['port']))
-		self.p1lock.release()
 	def __get_accept_val_from_acks(self):
 		# You don't have to check for depth, because it is checked on the process_prepare_msg. A node would NOT send an ACK if the depth is different from the prepared ballotNum. acceptVal/Num should be refreshed on every depth update.
 		maxAcceptVal = self.acceptVal
